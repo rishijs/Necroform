b2a932472cba13adfca96f716931a243f0929fa8
rj02sd <98123662+rj02sd@users.noreply.github.com>
2024-04-15 12:50:16 -0400

    level structure

---
+++ ui_elements/main_menu.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+
+func _ready():
+	pass
+
+
+func _process(_delta):
+	pass
+
+
+func _on_next_pressed():
+	get_tree().change_scene_to_file("res://levels/levels/level1.tscn")
--- ui_elements/victory.gd
+++ ui_elements/victory.gd
@@ -1,24 +1,22 @@
 extends CanvasLayer
 
 @export var level_index = 0
+var last_level = 4
 
 var levels = [
-	preload("res://levels/levels/level1.tscn"),
-	#preload("res://levels/levels/level2.tscn"),
-	#preload("res://levels/levels/level3.tscn"),
-	#preload("res://levels/levels/level4.tscn"),
-	#preload("res://levels/levels/level5.tscn"),
-	#preload("res://levels/levels/level6.tscn"),
-	#preload("res://levels/levels/level7.tscn"),
-	#preload("res://levels/levels/level8.tscn")
+	"res://levels/levels/level1.tscn",
+	"res://levels/levels/level2.tscn",
+	"res://levels/levels/level3.tscn",
+	"res://levels/levels/level4.tscn",
 ]
 
 func _ready():
 	pass
 
 
-func _process(delta):
-	pass
+func _process(_delta):
+	if level_index == 4 and visible:
+		get_tree().change_scene_to_file("res://levels/ending.tscn")
 
 
 func _on_next_pressed():
